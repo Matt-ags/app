6f7ab053bd105aaed42c129784000b979c87b391
Mateus <mateus.com>
2024-09-11 07:54:34 -0300

    fundamentos 005 - menu da aplicação iniciado e uso do async await

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-function start() {
+const { select } = require('@inquirer/prompts')
+
+const start = async() => {
 
     while(true){
-        let opcao = "sair"
+
+        //esse await (pra funcionar, tenha o async na função) fala pro js, "espera, o usuário vai selecionar algo!"
+
+        const opcao = await select ({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar",
+                },
+
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+
+        ///como está funcionando? Imagine o seguinte, a gente pegou o módulo do select, e esperou o usuário selecionar algo, se ele selecionou por exemplo cadastrar, opcao troca por cadastrar, e vai pro swith case, no cadastrar tem o caso de cadastrar, ele fasz o que tem, que fazer, terminando, volta no menu, e assim por diante. 
 
         // alterando a opcao com algum item abaixo, ele faz certa ação, mas toma cuidado que neste estado, se eu colocar em um, elke faz infinitamente:
 
@@ -15,6 +40,7 @@ function start() {
                 break
             
             case "sair":
+                console.log("Asta la vista, baby!")
                 return
     }
 }
