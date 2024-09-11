955ff8d849cdb74d68ba9879be3e49e20103c715
Mateus <mateus.com>
2024-09-10 21:26:03 -0300

    fundamentos 004 - uso do swith case

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function start() {
-    let count = 1
-    while(count<=10){
-        console.log(count)
-        count = count + 1
+
+    while(true){
+        let opcao = "sair"
+
+        // alterando a opcao com algum item abaixo, ele faz certa ação, mas toma cuidado que neste estado, se eu colocar em um, elke faz infinitamente:
+        
+        switch(opcao){
+            case "cadastrar":
+                console.log("vamos cadastrar!")
+                break
+            
+            case "listar":
+                console.log("vamos listar")
+                break
+            
+            case "sair":
+                return
     }
+}
+
 }
 
 start()
